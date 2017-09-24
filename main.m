b6f9d4df625a9f677900fbe88e6ee142c1cced26
haoliu <haoliu@1073828451@qq.com>
2017-09-24 09:32:43 +0800

    删除保存结果代码

--- main.m
+++ main.m
@@ -1,7 +1,7 @@
 %****************************************************************************************************
 % Author: Hao Liu and Yong Wang
 % Last Edited: 9/24/2017
-% Email: haoliu@csu.edu.cn@163.com; ywang@csu.edu.cn
+% Email: haoliu@csu.edu.cn; ywang@csu.edu.cn
 
 % Reference: Y. Wang, H. Liu, H. Long, Z. Zhang and S. Yang. Differential Evolution with A New Encoding
 % Mechanism for Optimizing Wind Farm Layout,
@@ -10,59 +10,47 @@
 
 clc;
 clear;
+tic;
 
 %The defined parameters
-interval = 15;                      %the angle interval
-interval_num = fix(360/interval);   % the number of bins
-input_filename='scenario1.xlsx';    %the data input file
-output_filename='data1.xlsx';       %the data output file
-X=2000;                             %the length of wind farm
-Y=2000;                             %the width of wind farm
-R=40;                               %the rotor radium
-H=80;                               %the hub height 
-CT=0.8;                             %the thrust coefficient
-a=1-sqrt(1-CT);                     %the axial induction factor * 2
-kappa=0.01;             %the spreading constant for land case(z=80,zo=0.54)
+interval = 15;                        %the angle interval
+interval_num = fix(360/interval);     %the number of bins
+X = 2000;                             %the length of wind farm
+Y = 2000;                             %the width of wind farm
+R = 40;                               %the rotor radium
+H = 80;                               %the hub height 
+CT = 0.8;                             %the thrust coefficient
+a = 1 - sqrt(1 - CT);                 %the axial induction factor * 2
+kappa = 0.01;                         %the spreading constant for land case(z=80,zo=0.54)
+cut_in_speed = 3.5;                   %the value of cut-in speed 
+rated_speed = 14;                     %the value of rated speed 
+cut_out_speed = 25;                   %the value of cut-out speed 
+minDistance = 5 * R;             %minimum distance between any two wind turbines
 
-%load the wind speed data
-
-
-%change natural wind speed into effective wind speed
-cut_in_speed=3.5;              %the value of cut-in speed 
-rated_speed=14;                %the value of rated speed 
-cut_out_speed=25;              %the value of cut-out speed 
-
-% global k c          %values of parameters k and c in weibull distribution
-k(1:interval_num)=2;
-c=[7 5 5 5 5 4 5 6 7 7 8 9.5 10 8.5 8.5 6.5 4.6 2.6 8 5 6.4 5.2 4.5 3.9];
-fre=[0.0003	0.0072	0.0237	0.0242	0.0222	0.0301	0.0397	0.0268	0.0626 ...	
+%Values of parameters k and c in weibull distribution and the frequency associated with each wind direction interval
+k(1:interval_num) = 2;
+c = [7 5 5 5 5 4 5 6 7 7 8 9.5 10 8.5 8.5 6.5 4.6 2.6 8 5 6.4 5.2 4.5 3.9];
+fre = [0.0003	0.0072	0.0237	0.0242	0.0222	0.0301	0.0397	0.0268	0.0626 ...	
     0.0801	0.1025	0.1445	0.1909	0.1162	0.0793	0.0082	0.0041	0.0008 ...	
     0.0010	0.0005	0.0013	0.0031	0.0085	0.0222];
 
-fprintf('The interval of every bin is %d\n',interval);
-fprintf('The number of bins is %d\n',interval_num);
-fprintf('The frequency in every bin are\n');
-fprintf('%.4f\t',fre);
-fprintf('\n');
+%If you want to test DEEM in wind scenario 2, please uncomment the following code
+%{
+k(1:interval_num) = 2;
+c(1:interval_num) = 13;
+fre = [0,0.01,0.01,0.01,0.01,0.2,0.6,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0.01,0];
+%}
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %population%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 populationsize=1;         %the number of individuals 
 maxEvaluations = 150000;
-minimumDistance1 = 5*R;%进化过程中最小距离
+
 repeatTimes = 10;%每种情况跑5次
 diffN = [15,20,25,30,35,40,60,80,100];%different number of individuals 
 diffSideLength = [2000,2000,2000,2200,2400,2600,3100,3600,4000];
 cases = length(diffN);
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%保存实验结果%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-resultRecordT = zeros(1,repeatTimes);
-saveE = zeros(cases,repeatTimes);
-saveStatisticalData = zeros(cases,5);
-saveCurve = [];
-saveLayout = cell(1,cases);
 
 for indexN = 1:cases
     N = diffN(indexN);
@@ -166,7 +154,7 @@ while(evaluations < maxEvaluations)
     candidate(1:2) = [];
     
     presentxChildren = mutationOperatorExecuteTIE(newpoint, presentxChildren,N,...,
-                       X,Y,minimumDistance1);
+                       X,Y,minDistance);
    
     thetaVeldefijBackup = thetaVeldefijMatrix;
     totalEnergyChildren = fitness(interval_num,interval,fre,N,presentxChildren(1,:), ...,
@@ -195,10 +183,6 @@ while(evaluations < maxEvaluations)
     evaluations = evaluations + 1; 
     
     if(~rem(evaluations,100))
-%     figure(1);
-%     print_turbine2(N,X,Y,presentxChildren(1,:));
-%     figure(2);
-%     plot(result);
       fprintf('The N is %d\n',N);
       fprintf('The cycle  is %d\n',cycle);
       fprintf('The evaluations  is %d\n',evaluations);
@@ -208,17 +192,8 @@ end
 toc;
 
 %记录单次实验结果
-resultRecordT(1,cycle) = toc;
-saveE(indexN,cycle) = resulttemp;
-saveCurve(cycle,:,indexN) = result;
-saveLayout{indexN}(cycle,:) = presentx(1,:);
+toc;
 % print_turbine2(N,X,Y,presentx(1,:));
 % plot(result);
 end%实验次数循环
-[saveStatisticalData(indexN,1),saveStatisticalData(indexN,4)] = max(saveE(indexN,:));%最大值及其索引
-saveStatisticalData(indexN,2) = mean(saveE(indexN,:));
-saveStatisticalData(indexN,3) = std(saveE(indexN,:));
-saveStatisticalData(indexN,5) = mean(resultRecordT);
-save DEEMdata saveCurve saveLayout saveE saveStatisticalData;
 end%风机个数循环
-%save dataFDE saveCurve saveLayout saveE saveStatisticalData;
